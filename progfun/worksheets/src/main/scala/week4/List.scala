3ffd491c94a32fcbdd313c3c3f5eadfce4a302b1
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-03-24 03:26:01 +0300

    Week4: Covariant List class.

--- progfun/worksheets/src/main/scala/week4/List.scala
+++ progfun/worksheets/src/main/scala/week4/List.scala
@@ -4,17 +4,18 @@ package week4
   * Created by mike on 29.03.16.
   */
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+  def prepend[ U >: T](elem: U): List[U] = new Cons(elem, this)
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
@@ -23,4 +24,8 @@ class Nil[T] extends List[T] {
 object List {
   //List(1,2)
   def apply[T](x1: T, x2: T):List[T] = new Cons(x1, new Cons(x2, new Nil))
+}
+
+object test {
+  val x: List[String] = Nil
 }
