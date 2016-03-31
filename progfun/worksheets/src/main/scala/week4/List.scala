a58404313d4563963e9b7eebbdf984b55a1c6669
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-04-01 00:49:05 +0300

    Week4: Fix lists.

--- progfun/worksheets/src/main/scala/week4/List.scala
+++ progfun/worksheets/src/main/scala/week4/List.scala
@@ -23,7 +23,7 @@ object Nil extends List[Nothing] {
 
 object List {
   //List(1,2)
-  def apply[T](x1: T, x2: T):List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T](x1: T, x2: T):List[T] = new Cons(x1, new Cons(x2, Nil))
 }
 
 object test {
